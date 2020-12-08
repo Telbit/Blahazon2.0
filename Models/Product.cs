3f48744f345344a2d0ed37664766a8f0e4f78972
J <J@DESKTOP-K8E0BDM>
2020-12-08 14:47:41 +0100

    models folder with basic Product model class

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blahazon2._0.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+    }
+}
