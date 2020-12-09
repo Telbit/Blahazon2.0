0bb3f12e77cc0d26b48a80470ae3edb33f03a32c
Telbit <t.t.bende@gmail.com>
2020-12-09 11:29:29 +0100

    some basic data

--- Models/ProductContext.cs
+++ Models/ProductContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,20 @@ namespace Blahazon2._0.Models
         public ProductContext(DbContextOptions<ProductContext> options)
             : base(options)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Products.Add(new Product
+                {
+                    Title = $"Title {i}",
+                    Description = $"Description {i}",
+                    ShortDescription = $"Short {i}",
+                    Type = $"Type {i}",
+                    InStock = true,
+                    Price = 1000,
+                    ImagePath = $"{AppDomain.CurrentDomain.BaseDirectory}images\\retard.png"
+                });
+                SaveChanges();
+            }
         }
 
     }
