24f1fbe7b10185ff66dedb01c7d079b361cf228c
J <J@DESKTOP-K8E0BDM>
2020-12-10 12:51:19 +0100

    added cartController to handle api requests

---
+++ Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Blahazon2._0.Models;
+
+namespace Blahazon2._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        // misleading name
+        private readonly ICart _cart;
+
+        public CartController(ICart cart)
+        {
+            _cart = cart;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetCart()
+        {
+            return _cart.GetCart();
+        }
+
+        [HttpPost]
+        public ActionResult<Product> AddToCart(Product product)
+        {
+            _cart.AddProduct(product);
+
+            return product;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Product> DeleteFromCart(long id)
+        {
+            _cart.DeleteProduct(id);
+
+            return NoContent();
+        }
+    }
+}
