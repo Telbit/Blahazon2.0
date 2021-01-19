36c57f3d6f92efe4ccfbe85bec8a4ef46c121774
Tomporino <tomorpeter97@gmail.com>
2021-01-19 13:56:02 +0100

    Lineitem

---
+++ Models/LineItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blahazon.Models
+{
+    public class LineItem : ILineItem
+    {
+        public int Quantity { get; set; }
+        public Product CurrentProduct { get; set; }
+
+        public void IncreaseQuantity() 
+        {
+            Quantity++;
+        }
+        public void DecreaseQuantity() 
+        {
+            Quantity--;
+        }
+        public decimal GetTotalPrice() 
+        {
+            return Quantity * CurrentProduct.Price;
+        }
+    }
+}
