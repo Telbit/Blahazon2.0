54f3a9d058acaf6040ec83422f9ff640de2c180c
Tomporino <tomorpeter97@gmail.com>
2021-01-21 20:46:20 +0100

    some styling for login and registration form

--- ClientApp/src/components/User/Login.js
+++ ClientApp/src/components/User/Login.js
@@ -1,24 +1,43 @@
 import {React, useState, useEffect} from 'react';
-import { FormControl, FormHelperText, InputLabel, Input, Card, Button } from '@material-ui/core';
+import { FormControl, FormHelperText, InputLabel, Input, Card, Button, Grid } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles'
 
+
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: 50
+    },
+}));
 
 export default function Login()
 {
+    const classes = useStyles();
     return(
     <>
-        <Card>
-            <FormControl>
+    <div className={classes.container}>
+        <Card align="center">
+            <Grid container 
+            spacing={5} 
+            >
+            <Grid item xs={12}>
+                <FormControl>
                     <InputLabel htmlFor="">Your Username</InputLabel>
                     <Input id="username-input" aria-describedby="helper-text"></Input>
                     <FormHelperText id='helper-text'>Never share your credentials</FormHelperText>
                 </FormControl>
-            <FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl>
                     <InputLabel htmlFor="">Your password</InputLabel>
                     <Input id="password-input" aria-describedby="helper-text"></Input>
                     <FormHelperText id='helper-text'>Never share your credentials</FormHelperText>
-            </FormControl>
+                </FormControl>
+            </Grid>
+            </Grid>
+            <Button>Sign in</Button>
         </Card>
-
-        <Button>Sign in</Button>
-    </>)
+    </div>
+    </>
+    )
 }
