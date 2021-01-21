9952db8d0d9f6ab84640c0cfbb8e50dc0389beb0
allused <divixed12@gmail.com>
2021-01-21 15:03:05 +0100

    Cart testing frontend component

---
+++ ClientApp/src/components/DbTest/CartTest.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+const [product, setProduct] = useState(null);
+
+function CartTest(props) {
+
+    getProduct = () => { axios('https://localhost:44309/api/Cart/1')
+                                    .then(res => setProduct(res))
+                                }
+    useEffect(() => {
+        axios.post('https://localhost:44309/api/Cart/1',
+        {
+            "Id":2,
+            "Title":"pol",
+            "Type":"t-shirt",
+            "ShortDescription":"rovid leiras",
+            "Description":"Leiras",
+            "Price":6500,
+            "InStock":1,
+            "ImagePath":"imagesrc"
+        }
+        )
+        .then(getProduct)
+        
+    }, [])
+
+
+
+
+    return (
+        <div>
+            <p>{product == null ? "Loading" : product.Title}</p>
+        </div>
+    )
+}
+
+
+export default DbTest
+
