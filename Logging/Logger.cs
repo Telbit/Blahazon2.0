1d3fd6e651b5e65029071ab5cc85dcf160ee3064
allused <divixed12@gmail.com>
2021-02-03 15:08:38 +0100

    created logger class

---
+++ Logging/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Blahazon.Logging
+{
+    public static class Logger
+    {
+        private static string MachineNamee { get; set; }
+        public static void SetupLogger(string machineName)
+        {
+            MachineNamee = machineName;
+            if (!EventLog.SourceExists("BlahaLogger", MachineNamee))
+            {
+                EventLog.CreateEventSource(new EventSourceCreationData("BlahaLogger", "localhost") { MachineName = MachineNamee });
+            }
+        }
+
+        public static EventLog GetNewEventLog()
+        {
+            return new EventLog("localhost", MachineNamee, "BlahaLogger");
+        }
+    }
+}
