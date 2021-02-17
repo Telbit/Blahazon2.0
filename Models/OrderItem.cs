e8579659a641afe938140e1f5406b8c0fa2db91e
allused <divixed12@gmail.com>
2021-02-17 20:24:17 +0100

    added OrderItem model to support email service

---
+++ Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blahazon.Models
+{
+    public struct OrderItem
+    {
+        public string ProductTitle { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
